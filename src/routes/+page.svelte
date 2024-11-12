dff7dd53fe39ef0ee3aacb9a4149d6be79e29675
Oleksandr Lenskyi <olelen@elevad.umea.se>
2024-11-12 18:55:23 +0100

    installation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,25 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+<p id="inspo">
+    "The only way to learn a new programming language is by writing programs in it." - <strong>Dennis Ritchie</strong>, creator of the C programming language.
+</p>
+
+<p style="background-color:pink">
+    "Every great developer you know got there by solving problems they were <block>bro</block> unqualified to solve until they actually did it." - <span>Patrick McKenzie, software engineer and entrepreneur</span>.
+</p>
+.
+.
+.
+<style>
+#inspo{
+    font-family: courier;
+    font-size: 160%;
+}
+
+strong{
+  color: cyan;
+}
+
+span{
+    color: green;
+}
+</style>
